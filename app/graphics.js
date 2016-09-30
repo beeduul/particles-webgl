f1dd9bfed2bf49b0740e7b42355606083c628e25
Jeremy Biddle <biddle@adobe.com>
2016-09-29 23:31:35 -0700

    create webgl canvas using canvasAttributes

--- app/graphics.js
+++ app/graphics.js
@@ -801,7 +801,14 @@ function initWebGL(canvas) {
 
   try {
   // Try to grab the standard context. If it fails, fallback to experimental.
-    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    var contextAttributes = {antialias: false};
+    gl = canvas.getContext("webgl", contextAttributes);
+    if (gl) {
+      console.log("webgl context creation succeeded with contextAttributes ", contextAttributes);
+    } else {
+      console.log("webgl context creation failed, falling back to experimental-webgl");
+      canvas.getContext("experimental-webgl", contextAttributes);
+    }
   }
   catch(e) {}
 

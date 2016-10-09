5452501c3d437b8e81b66c57c9300ff42ea9623d
Jeremy Biddle <biddle@adobe.com>
2016-10-08 20:38:28 -0700

    generate colors in hsv, ensure a minimum vibrance

--- app/graphics.js
+++ app/graphics.js
@@ -227,10 +227,55 @@ var Graphics = {
 
   },
 
+  randomHSV: function() {
+    var min_s = 0.75;
+    var min_v = 0.75;
+
+    var h = Math.random() * 360; // any hue
+    var s = Math.random() * (1 - min_s) + min_s;
+    var v = Math.random() * (1 - min_v) + min_v;
+    
+    return [h, s, v];
+  },
+
+  hsvToRgb: function(hsv) {
+    
+    var h = hsv[0];
+    var s = hsv[1];
+    var v = hsv[2];
+    
+    var c = v * s;
+    var x = c * (1 - Math.abs((h / 60) % 2 - 1));
+    var m = v - c;
+
+    while (h < 0) h += 360;
+    while (h > 360) h -= 360;
+
+    var r, g, b;
+    if (0 <= h && h < 60) {
+      r = c; g = x; b = 0;
+    } else if (60 <= h && h < 120) {
+      r = x; g = c; b = 0;
+    } else if (120 <= h && h < 180) {
+      r = 0; g = c; b = x;
+    } else if (180 <= h && h < 240) {
+      r = 0; g = x; b = c;
+    } else if (240 <= h && h < 300) {
+      r = x; g = 0; b = c;
+    } else if (300 <= h && h < 360) {
+      r = c; g = 0; b = x;
+    } else {
+      throw "invalid h value: " + h;
+    }
+    
+    return [r + m, g + m, b + m];
+  },
+
   handleMouseEvent: function(event) {
+
     if (event.type == "mousedown") {
       // event.preventDefault();
-      this.rgb = [Math.random(), Math.random(), Math.random()]
+      this.rgb = this.hsvToRgb(this.randomHSV());
       this.addParticlesAt(new glMatrix.vec2.fromValues(event.x, event.y), this.rgb);
     } else if (event.type == "mousemove") {
       // event.preventDefault();
